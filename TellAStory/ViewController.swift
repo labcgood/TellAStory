edc4ed34e94a22818677d2fd30948892ef6fd137
Rabbit <labcgood44@gmail.com>
2024-01-04 14:46:27 +0800

    Initial Commit

---
+++ TellAStory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TellAStory
+//
+//  Created by Labe on 2024/1/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
